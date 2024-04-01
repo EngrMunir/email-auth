9c0530a5fd1ef8854fff6a8069dd328ca1116613
Munir <smsirajulmonir@gmail.com>
2024-04-01 14:23:03 +0600

    basic setup ready

---
+++ src/Components/Register/Register.jsx
@@ -0,0 +1,13 @@
+
+const Register = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">Please Register</h2>
+            <form>
+                <input type="email" name="email" id="" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
